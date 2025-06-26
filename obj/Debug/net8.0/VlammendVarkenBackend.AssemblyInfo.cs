3b5e2ac6e800cf30deab38b711cd3a895e00082d
mjkboegheim <2205442boegheim@zuyd.nl>
2025-06-26 16:23:16 +0200

    New Update.

--- obj/Debug/net8.0/VlammendVarkenBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/VlammendVarkenBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VlammendVarkenBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef499c0569a7ab2e1f301d4ab8bd5b7021b00f1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac08ca87860f0c1c4981044df05fe8ec52533b3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("VlammendVarkenBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VlammendVarkenBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
