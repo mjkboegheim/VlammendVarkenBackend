a56f6c0460d22c21dd9d648606c41ee393aca719
mjkboegheim <2205442boegheim@zuyd.nl>
2025-06-27 10:15:54 +0200

    Overzicht tabellen verwijderd.

--- obj/Debug/net8.0/VlammendVarkenBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/VlammendVarkenBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VlammendVarkenBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+793c221ea7d2793207bb1b38b840a77c62986dc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a600db5e53b411f6730d783aab30dca421a54bad")]
 [assembly: System.Reflection.AssemblyProductAttribute("VlammendVarkenBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VlammendVarkenBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
